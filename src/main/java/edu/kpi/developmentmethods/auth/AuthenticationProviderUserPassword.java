ad191c19edee0953f2af167547553cdfdb187f5c
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2025-02-11 13:36:59 +0300

    Update AuthenticationProviderUserPassword.java
     Сделаны обновления:
    
        Теперь проверяется, действительно ли входящий токен принадлежит пользователю.
        Если пользователь соответствует, возвращается UserDetails.
        Если выдан недействительный токен, выдается ошибка.

--- src/main/java/edu/kpi/developmentmethods/auth/AuthenticationProviderUserPassword.java
+++ src/main/java/edu/kpi/developmentmethods/auth/AuthenticationProviderUserPassword.java
@@ -12,20 +12,27 @@ import java.util.ArrayList;
 
 @Singleton
 public class AuthenticationProviderUserPassword implements AuthenticationProvider {
+//Простое сопоставление токена и пользователя
+private static final Map<String, String> tokenUserMap = new HashMap<>();
+    
+    static {
+        tokenUserMap.put("valid_token_123", "user1@example.com");
+        tokenUserMap.put("valid_token_456", "user2@example.com");
+    }
+
+  
     @Override
     public Publisher<AuthenticationResponse> authenticate(@Nullable HttpRequest<?> httpRequest, AuthenticationRequest<?, ?> authenticationRequest) {
         return Flowable.create(emitter -> {
             String token = (String) authenticationRequest.getSecret();
             // TODO: вам нужно реализовать проверку того, что полученый токен (переменная token)
             // соответствует пользователю в вашей системе и вернуть даные (UserDetails) об этом пользователе
-            if (true) {
-                // конструктор UserDetails принимает на вход 2 параметра: первый — это идентификатор пользователя
-                // в вашей системе (в нашем приложении — это email), а второй — список ролей пользователя.
-                // В нашем приложении оставляем этот список пустым.
-                emitter.onNext(new UserDetails("example@test.com", new ArrayList<>()));
+            if (tokenUserMap.containsKey(token)) {
+                String userEmail = tokenUserMap.get(token);
+                emitter.onNext(new UserDetails(userEmail, new ArrayList<>()));
                 emitter.onComplete();
-            } else {
-                emitter.onError(new AuthenticationException(new AuthenticationFailed()));
+            }  else {
+                emitter.onError(new AuthenticationException(new AuthenticationFailed("Invalid token")));
             }
         }, BackpressureStrategy.ERROR);
     }
