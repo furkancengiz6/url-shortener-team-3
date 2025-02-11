e19bd674a70e35ce257bd33f84a075c09f2dea08
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2025-02-11 13:57:39 +0300

    Update InMemoryStorageImpl.java
    Файловое хранилище: Данные записываются в файл с именем storage.dat и считываются из него.
    
        Мы сохраняем данные в файл с помощью ObjectOutputStream.
        Для чтения из файла используется ObjectInputStream.
    
    Сохранение данных: Этот класс сохраняет данные в файл после каждой вставки или удаления данных. При перезапуске приложения данные перезагружаются из файла.

--- src/main/java/edu/kpi/developmentmethods/storage/InMemoryStorageImpl.java
+++ src/main/java/edu/kpi/developmentmethods/storage/InMemoryStorageImpl.java
@@ -2,28 +2,56 @@ package edu.kpi.developmentmethods.storage;
 
 import edu.kpi.developmentmethods.dto.Example;
 
+import java.io.*;
 import java.util.HashMap;
 
-/**
- * Реализация хранилища в памяти.
- * Вам нужно реализовать персистентное хранилище, т.е. даные не должны
- * удаляться при перезапуске приложения.
- */
-public class InMemoryStorageImpl implements Storage {
-    private HashMap<String, Example> map = new HashMap<>();
+public class PersistentStorageImpl implements Storage {
+    private static final String FILE_NAME = "storage.dat";
+    private HashMap<String, Example> map;
+
+    public PersistentStorageImpl() {
+        this.map = loadFromFile();
+    }
 
     @Override
     public void del(String key) {
         this.map.remove(key);
+        saveToFile();
     }
 
     @Override
     public void put(String key, Example value) {
         this.map.put(key, value);
+        saveToFile();
     }
 
     @Override
     public Example get(String key) {
         return this.map.get(key);
+    }
+
+   
+    private void saveToFile() {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
+            out.writeObject(map);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+   
+    @SuppressWarnings("unchecked")
+    private HashMap<String, Example> loadFromFile() {
+        File file = new File(FILE_NAME);
+        if (!file.exists()) {
+            return new HashMap<>();
+        }
+
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(file))) {
+            return (HashMap<String, Example>) in.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return new HashMap<>();
+        }
     }
 }
