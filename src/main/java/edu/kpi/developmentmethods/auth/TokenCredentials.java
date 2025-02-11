f781804def4ee23ab93f204ff5e017762c09578b
Furkan Cengiz <76488131+furkancengiz6@users.noreply.github.com>
2025-02-11 19:46:10 +0300

    Update TokenCredentials.java

--- src/main/java/edu/kpi/developmentmethods/auth/TokenCredentials.java
+++ src/main/java/edu/kpi/developmentmethods/auth/TokenCredentials.java
@@ -1,20 +1,18 @@
 package edu.kpi.developmentmethods.auth;
 
 import io.micronaut.security.authentication.AuthenticationRequest;
-
 import java.io.Serializable;
 
 public class TokenCredentials implements Serializable, AuthenticationRequest<String, String> {
     private final String token;
 
     TokenCredentials(String token) {
-        super();
         this.token = token;
     }
 
     @Override
     public String getIdentity() {
-        return null;
+        return "Token User"; 
     }
 
     @Override
